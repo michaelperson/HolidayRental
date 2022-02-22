69fc3656214e4b26c2b1965a5ae9c06f5519a9d7
Person M <michael.person@cognitic.be>
2022-02-22 11:29:45 +0100

    Ajout des interfaces pour les entités et les repositories

---
+++ HoliDayRental.DAL.Interfaces/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HoliDayRental.DAL.Interfaces
+{
+    /// <summary>
+    /// Permet de definir quelle est la  proprieté pour la Primary Key dans la DB
+    /// </summary>
+    /// <typeparam name="TKey">Le type de l'identifiant</typeparam>
+    public interface IEntity<TKey>
+        where TKey : struct
+    {
+        /// <summary>
+        /// Represente la PK dans la DB
+        /// </summary>
+        TKey ID { get; }
+    }
+}
+
